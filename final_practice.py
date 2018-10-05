239f0a31fe53bbfc8b6b85b5ac978387ae51704a
brnnmchl <brinnamichael@wirelessprv-10-194-22-112.near.illinois.edu>
2018-10-05 16:12:54 -0500

    final practice initial commit
    this is the first version of my practice .py code for the final project

---
+++ final_practice.py
@@ -0,0 +1,24 @@
+# final practice.py
+
+import time
+
+print('For years, you have studied and trained and now\n'
+      'the time has arrived at last. You approach the\n'
+      'intimidating oak doors and push them open...\n\n')
+time.sleep(5)
+print('Welcome, to LIBRARY QUEST!\n'
+      '\u2669 \u266B \u2669 \u266B \u2669 \u266B \u2669 \u266B \u2669 \u266B \u2669 \u266B \u2669 \u266B\n\n')
+time.sleep(2)
+print('You are greeted inside the doors by an elderly woman.\n\n')
+time.sleep(3)
+player_name = input('AGNES: Welcome to the Library. I am Agnes.\n'
+                    'What can I call you? >').title()
+time.sleep(1)
+print("\n\nAGNES: It's a pleasure to meet you", player_name, ".\n\n")
+time.sleep(1)
+print("AGNES: This must be your first day. Well, don't\n"
+            "worry, we here at the Library are all quite\n"
+            "friendly. I'm sure you'll enjoy working here.\n\n")
+
+
+
